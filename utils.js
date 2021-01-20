17356dc16fdc92ac385553d1903ef0a92c85659d
harriet-the-spy <dpcairns@gmail.com>
2021-01-20 12:05:19 -0800

    add alternative

--- utils.js
+++ utils.js
@@ -3,6 +3,19 @@ export function findById(id, array) {
     // iterate/loop through the array
     for (let item of array) {
         // compare our id to the id of the item
+        if (item.id === id) {
+            // if they match, return that item
+            return item;
+        }
+    }
+}
+
+// we will assume valid input: an array with objects that all have an id
+export function alternativeFindById(id, array) {
+    // iterate/loop through the array
+    for (let i = 0; i < array.length; i++) {
+        const item = array[i];
+        // compare our id to the id of the item
         if (id === item.id) {
             // if they match, return that item
             return item;
