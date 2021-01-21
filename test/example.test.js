8a378be7105d5d7e2250a527a8ccb7b7051048f2
harriet-the-spy <dpcairns@gmail.com>
2021-01-21 14:35:15 -0800

    add test for getCart

--- test/example.test.js
+++ test/example.test.js
@@ -2,6 +2,7 @@
 import { renderAnimal } from '../products/render-animal.js';
 import { findById } from '../utils.js';
 import { animals } from '../animals.js';
+import { setCart, getCart } from '../cart-utils.js';
 // laboratory for our function
 
 
@@ -32,77 +33,97 @@ const test = QUnit.test;
 //     expect.equal(actual.outerHTML, expected);
 // });
 
-test('findById should take in an 2 and the animals array and return the dog', (expect) => {
-    // const animals = [
-    //     {
-    //         id: 1,
-    //         price: 40,
-    //         image: 'iguana.png',
-    //         legs: 4,
-    //         species: 'iguana',
-    //         size: 'huge',
-    //         name: 'Freddy Guana',
-    //         hasFur: false,
-    //     },
-    //     {
-    //         id: 2,
-    //         price: 50,
-    //         image: 'dog.png',
-    //         legs: 4,
-    //         species: 'dog',
-    //         size: 'medium',
-    //         name: 'Phillip',
-    //         hasFur: true,
-    //     },
-    //     {
-    //         id: 3,
-    //         price: 20,
-    //         image: 'pig.png',
-    //         legs: 4,
-    //         species: 'pig',
-    //         size: 'small',
-    //         name: 'Willow-Suzie',
-    //         hasFur: false,
-    //     },
-    //     {
-    //         id: 4,
-    //         price: 10,
-    //         image: 'trash-panda.png',
-    //         legs: 4,
-    //         species: 'trash-panda',
-    //         size: 'medium',
-    //         name: 'Cassidy',
-    //         hasFur: true,
-    //     },
-    //     {
-    //         id: 5,
-    //         price: 90,
-    //         image: 'red-panda.png',
-    //         legs: 4,
-    //         species: 'red-panda',
-    //         size: 'small',
-    //         name: 'Penny',
-    //         hasFur: true,
-    //     },
-    // ];
-    // //Arrange
-    // // Set up your arguments and expectations
-    const expected = {
-        id: 2,
-        price: 50,
-        image: 'dog.png',
-        legs: 4,
-        species: 'dog',
-        size: 'medium',
-        name: 'Phillip',
-        hasFur: true,
-    };
+// test('findById should take in an 2 and the animals array and return the dog', (expect) => {
+//     // const animals = [
+//     //     {
+//     //         id: 1,
+//     //         price: 40,
+//     //         image: 'iguana.png',
+//     //         legs: 4,
+//     //         species: 'iguana',
+//     //         size: 'huge',
+//     //         name: 'Freddy Guana',
+//     //         hasFur: false,
+//     //     },
+//     //     {
+//     //         id: 2,
+//     //         price: 50,
+//     //         image: 'dog.png',
+//     //         legs: 4,
+//     //         species: 'dog',
+//     //         size: 'medium',
+//     //         name: 'Phillip',
+//     //         hasFur: true,
+//     //     },
+//     //     {
+//     //         id: 3,
+//     //         price: 20,
+//     //         image: 'pig.png',
+//     //         legs: 4,
+//     //         species: 'pig',
+//     //         size: 'small',
+//     //         name: 'Willow-Suzie',
+//     //         hasFur: false,
+//     //     },
+//     //     {
+//     //         id: 4,
+//     //         price: 10,
+//     //         image: 'trash-panda.png',
+//     //         legs: 4,
+//     //         species: 'trash-panda',
+//     //         size: 'medium',
+//     //         name: 'Cassidy',
+//     //         hasFur: true,
+//     //     },
+//     //     {
+//     //         id: 5,
+//     //         price: 90,
+//     //         image: 'red-panda.png',
+//     //         legs: 4,
+//     //         species: 'red-panda',
+//     //         size: 'small',
+//     //         name: 'Penny',
+//     //         hasFur: true,
+//     //     },
+//     // ];
+//     // //Arrange
+//     // // Set up your arguments and expectations
+//     const expected = {
+//         id: 2,
+//         price: 50,
+//         image: 'dog.png',
+//         legs: 4,
+//         species: 'dog',
+//         size: 'medium',
+//         name: 'Phillip',
+//         hasFur: true,
+//     };
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = findById(2, animals);
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = findById(2, animals);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.deepEqual(actual, expected);
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.deepEqual(actual, expected);
+// });
+
+test('getCart should get the correct cart from local storage', (expect) => {
+    const testCart = [
+        { 
+            id: 3,
+            quantity: 2
+        },
+        { 
+            id: 4,
+            quantity: 1
+        },
+    ];
+    const stringyCart = JSON.stringify(testCart);
+
+    localStorage.setItem('CART', stringyCart);
+    
+    const cart = getCart();
+
+    expect.deepEqual(cart, testCart);
 });
